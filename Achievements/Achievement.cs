16e45de69381c1a7d56393f0d6e83e31c3f94813
sebingel <sebingel+github@googlemail.com>
2015-11-24 13:02:56 +0100

    - added some commentaries and a todo

--- Achievements/Achievement.cs
+++ Achievements/Achievement.cs
@@ -2,21 +2,53 @@
 
 namespace sebingel.Achievements
 {
+    /// <summary>
+    /// Class that represents an achievement
+    /// </summary>
     public class Achievement
     {
+        #region Properties
+
+        /// <summary>
+        /// List of conditions which must be met to unlock the achievement
+        /// </summary>
         public List<AchievementCondition> Conditions { get; private set; }
+
+        /// <summary>
+        /// Titel of the achievement
+        /// </summary>
         public string Titel { get; private set; }
+
+        /// <summary>
+        /// Description of the achievement
+        /// </summary>
         public string Description { get; private set; }
+
+        /// <summary>
+        /// Flag which shows if the achievement is unlocked
+        /// </summary>
         public bool Unlocked { get; private set; }
+
+        /// <summary>
+        /// Progress of unlocking the achievement
+        /// </summary>
         public int Progress { get; private set; }
 
+        #endregion
+
+        /// <summary>
+        /// Event that fires when the progress has changed
+        /// </summary>
         public event AchievementProgressChangedHandler ProgressChanged;
-        protected virtual void InvokeProgressChanged(int progressCount)
+        private void InvokeProgressChanged(int progressCount)
         {
             if (ProgressChanged != null)
                 ProgressChanged(this, new AchievementProgressChangedArgs(progressCount));
         }
 
+        /// <summary>
+        /// Event that fires when the achievement is unlocked
+        /// </summary>
         public event AchievementCompleteHandler AchievementCompleted;
         private void InvokeAchievementCompleted()
         {
@@ -25,6 +57,14 @@ namespace sebingel.Achievements
                 AchievementCompleted(this);
         }
 
+        #region Constructor(s)
+
+        /// <summary>
+        /// An achievement
+        /// </summary>
+        /// <param name="titel">Titel of the achievement</param>
+        /// <param name="description">Description of the achievement</param>
+        /// <param name="conditions">List of conditions which must be met to unlock the achievement</param>
         public Achievement(string titel, string description, List<AchievementCondition> conditions)
         {
             Conditions = conditions;
@@ -38,6 +78,12 @@ namespace sebingel.Achievements
             }
         }
 
+        #endregion
+
+        /// <summary>
+        /// This method is tied to the ConditionCompleted events in the AchievementConditions of this Achievement
+        /// </summary>
+        /// <param name="achievementCondition">AchievementCondition thath fired the event</param>
         private void ConditionCompleted(AchievementCondition achievementCondition)
         {
             if (Unlocked)
@@ -57,6 +103,11 @@ namespace sebingel.Achievements
             }
         }
 
+        /// <summary>
+        /// This method is tied to the ProgressChanged events in the AchievementConditions of this Achievement
+        /// </summary>
+        /// <param name="sender">AchievementCondition thath fired the event</param>
+        /// <param name="args">Parameters that are important for this event</param>
         private void ConditionProgressChanged(AchievementCondition sender, AchievementConditionProgressChangedArgs args)
         {
             if (sender.Unlocked)
@@ -64,6 +115,7 @@ namespace sebingel.Achievements
                 return;
             }
 
+            // TODO: Calculate exact progress
             Progress = 100;
 
             InvokeProgressChanged(Progress);
--- Achievements/AchievementCompleteHandler.cs
+++ Achievements/AchievementCompleteHandler.cs
@@ -1,4 +1,8 @@
 ﻿namespace sebingel.Achievements
 {
+    /// <summary>
+    /// Delegate for an event that fires when the achievement is unlocked
+    /// </summary>
+    /// <param name="achievement">The achievement that fired the event</param>
     public delegate void AchievementCompleteHandler(Achievement achievement);
 }
