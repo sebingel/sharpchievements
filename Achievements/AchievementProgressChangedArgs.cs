630eaaa14161a6cec7179fef5004d9d97f73ef1a
sebingel <sebingel+github@googlemail.com>
2015-11-25 13:18:46 +0100

    - added some more comments

--- Achievements/AchievementProgressChangedArgs.cs
+++ Achievements/AchievementProgressChangedArgs.cs
@@ -1,9 +1,19 @@
 ﻿namespace sebingel.scharpchievements
 {
+    /// <summary>
+    /// This class contains data that about the progresschange of an Achievement
+    /// </summary>
     public class AchievementProgressChangedArgs
     {
+        /// <summary>
+        /// Current progress of the Achievement
+        /// </summary>
         public int ProgressCount { get; private set; }
 
+        /// <summary>
+        /// This class contains data that about the progresschange of an Achievement
+        /// </summary>
+        /// <param name="progressCount">Current progress of the Achievement</param>
         public AchievementProgressChangedArgs(int progressCount)
         {
             ProgressCount = progressCount;
--- Achievements/AchievementProgressChangedHandler.cs
+++ Achievements/AchievementProgressChangedHandler.cs
@@ -1,4 +1,9 @@
 ﻿namespace sebingel.scharpchievements
 {
+    /// <summary>
+    /// Delegate of the event that fires when the progress of an Achievement is changed
+    /// </summary>
+    /// <param name="sender">The Achievement which progress is changed</param>
+    /// <param name="args">Parameters containing further information of the progress change</param>
     public delegate void AchievementProgressChangedHandler(Achievement sender, AchievementProgressChangedArgs args);
 }
