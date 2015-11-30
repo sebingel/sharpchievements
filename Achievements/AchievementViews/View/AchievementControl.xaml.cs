f7367b10ae4ca6d2bc81209344e821b6c86c4250
sebingel <sebingel+github@googlemail.com>
2015-11-30 13:16:51 +0100

    - added AchievementControl and AchievementOverviewControl

---
+++ Achievements/AchievementViews/View/AchievementControl.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using sebingel.sharpchievements.Annotations;
+
+namespace sebingel.sharpchievements.AchievementViews.View
+{
+    /// <summary>
+    /// A control to display an Achievement
+    /// </summary>
+    public partial class AchievementControl : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// The Titel of the Achievement
+        /// </summary>
+        public string Titel { get; private set; }
+
+        /// <summary>
+        /// The Description of the Achievement
+        /// </summary>
+        public string Description { get; private set; }
+
+        /// <summary>
+        /// Path to the Image that is displayed in the notification
+        /// </summary>
+        public string ImagePath { get; private set; }
+
+        /// <summary>
+        /// Visibility of the image
+        /// </summary>
+        public Visibility ImageVisibility { get; private set; }
+
+        /// <summary>
+        /// Achievement to display
+        /// </summary>
+        public Achievement Achievement
+        {
+            set
+            {
+                if (value != null)
+                {
+                    Titel = value.Titel;
+                    Description = value.Description;
+                    ImagePath = value.ImagePath;
+
+                    if (String.IsNullOrEmpty(value.ImagePath))
+                        ImageVisibility = Visibility.Collapsed;
+                    else
+                        ImageVisibility = Visibility.Visible;
+
+                    InvokePropertyChanged("Titel");
+                    InvokePropertyChanged("Description");
+                    InvokePropertyChanged("ImagePath");
+                    InvokePropertyChanged("ImageVisibility");
+                }
+            }
+        }
+
+        /// <summary>
+        /// A control to display an Achievement
+        /// </summary>
+        public AchievementControl()
+            : this(null)
+        { }
+
+        /// <summary>
+        /// A control to display an Achievement
+        /// </summary>
+        /// <param name="achievement">Achievement to display</param>
+        public AchievementControl(Achievement achievement)
+        {
+            InitializeComponent();
+
+            Achievement = achievement;
+        }
+
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+        [NotifyPropertyChangedInvocator]
+        private void InvokePropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+    }
+}
---
+++ Achievements/AchievementViews/View/AchievementOverviewControl.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using sebingel.sharpchievements.Annotations;
+
+namespace sebingel.sharpchievements.AchievementViews.View
+{
+    /// <summary>
+    /// A basic overview of a set of Achievements
+    /// </summary>
+    public partial class AchievementOverviewControl : INotifyPropertyChanged
+    {
+        private List<Achievement> achievementList;
+
+        /// <summary>
+        /// A list containing Achievements that are displayed
+        /// </summary>
+        public List<Achievement> AchievementList
+        {
+            get
+            {
+                return achievementList;
+            }
+            set
+            {
+                achievementList = value;
+                InvokePropertyChanged();
+            }
+        }
+
+        /// <summary>
+        /// A basic overview of all registered Achievements
+        /// </summary>
+        public AchievementOverviewControl()
+            : this(null)
+        {
+            InitializeComponent();
+            AchievementOverViewControlAchievementsChanged();
+
+            // When called with no parameters the control will keep track of all registered Achievements by subscribing the according event in the AchievementManager
+            AchievementManager.GetInstance().AchievementsChanged += AchievementOverViewControlAchievementsChanged;
+        }
+
+        /// <summary>
+        /// A basic overview of a set of Achievements
+        /// </summary>
+        /// <param name="achievementList">A set of Achievements to be desplyed</param>
+        public AchievementOverviewControl(List<Achievement> achievementList)
+        {
+            InitializeComponent();
+            AchievementList = achievementList;
+        }
+
+        /// <summary>
+        /// Happens when an Achievement is changed in the AchievementManager
+        /// </summary>
+        private void AchievementOverViewControlAchievementsChanged()
+        {
+            AchievementList = new List<Achievement>(AchievementManager.GetInstance().GetAchievementList());
+
+            ic.Items.Clear();
+            foreach (Achievement achievement in AchievementList)
+            {
+                ic.Items.Add(new AchievementControl(achievement) { Margin = new Thickness(5) });
+            }
+        }
+
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+        [NotifyPropertyChangedInvocator]
+        protected virtual void InvokePropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+    }
+}
