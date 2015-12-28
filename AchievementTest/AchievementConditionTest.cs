66da9a38f3c2aa582b3ac8895fa46718c0dde53f
sebingel <sebingel+github@googlemail.com>
2015-12-28 15:35:08 +0100

    - revised testclass and methods

---
+++ AchievementTest/AchievementConditionTest.cs
@@ -0,0 +1,80 @@
+﻿using System.Diagnostics;
+using sebingel.sharpchievements;
+
+namespace AchievementTest
+{
+    /// <summary>
+    /// Test Class for AchievementCondition
+    /// </summary>
+    internal static class AchievementConditionTest
+    {
+        private static bool madeProgress;
+        private static bool completed;
+
+        private static string uniqueId = "acUniqueId";
+        private static string achievementConditionKey = "acConditionKey";
+        private static int countToUnlock = 5;
+
+        /// <summary>
+        /// Executes Tests for Creating an AchivementCondition, making some progress and completing an AchievementCondition
+        /// </summary>
+        internal static void Execute()
+        {
+            CreateAchievementCondition();
+            DoAchievementConditionProgress();
+            CompleteAchievementCondition();
+        }
+
+        private static void AcConditionCompleted(AchievementCondition achievementCondition)
+        {
+            completed = true;
+        }
+
+        private static void CompleteAchievementCondition()
+        {
+            AchievementCondition ac = GetNewAchievementCondition();
+            ac.ConditionCompleted += AcConditionCompleted;
+
+            ac.MakeProgress();
+            ac.MakeProgress();
+            ac.MakeProgress();
+            ac.MakeProgress();
+
+            Debug.Assert(madeProgress, "madeProgress");
+            Debug.Assert(!completed, "!completed");
+            Debug.Assert(!ac.Unlocked, "!ac.Unlocked");
+
+            ac.MakeProgress();
+
+            Debug.Assert(completed, "completed");
+            Debug.Assert(ac.Unlocked, "ac.Unlocked");
+        }
+
+        private static AchievementCondition GetNewAchievementCondition()
+        {
+            return new AchievementCondition(uniqueId, achievementConditionKey, countToUnlock);
+        }
+
+        private static void DoAchievementConditionProgress()
+        {
+            AchievementCondition ac = GetNewAchievementCondition();
+            ac.ProgressChanged += AcProgressChanged;
+            ac.MakeProgress();
+
+            Debug.Assert(madeProgress, "madeProgress");
+        }
+
+        private static void AcProgressChanged(AchievementCondition sender, AchievementConditionProgressChangedArgs args)
+        {
+            madeProgress = true;
+        }
+
+        private static void CreateAchievementCondition()
+        {
+            AchievementCondition ac = GetNewAchievementCondition();
+            Debug.Assert(ac.UniqueId == uniqueId, "ac.UniqueId==uniqueId");
+            Debug.Assert(ac.Unlocked == false, "ac.Unlocked==false");
+            Debug.Assert(ac.AchievementConditionKey == achievementConditionKey, "ac.AchievementConditionKey==achievementConditionKey");
+        }
+    }
+}
--- AchievementTest/Program.cs
+++ AchievementTest/Program.cs
@@ -1,99 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using sebingel.sharpchievements;
-
-namespace AchievementTest
+﻿namespace AchievementTest
 {
-    public class Program
+    public static class Program
     {
-        private static readonly AchievementManager Am = AchievementManager.GetInstance();
-
         static void Main()
         {
-            Am.AchievementCompleted += AchievementCompleted;
-
-            BasicTest();
-            //SaveLoadTest();
-        }
-
-        private static void SaveLoadTest()
-        {
-            Am.LoadAchievements(@"C:\Datasec\achievementTest.bin", true);
-            AchievementCondition ac = new AchievementCondition("titel", "key", 5);
-            Achievement a = new Achievement("a", "a", "desc", ac);
-
-            Am.RegisterAchievementCondition(ac);
-            Am.RegisterAchievement(a);
-
-            Am.ReportProgress("key");
-            Am.ReportProgress("key");
-            Am.ReportProgress("key");
-
-            Am.SaveAchiements(@"C:\Datasec\achievementTest.bin");
-
-            Console.ReadLine();
-        }
-
-        private static void BasicTest()
-        {
-            AchievementCondition achievementCondition = new AchievementCondition("aCondition", "a", 5);
-            AchievementCondition condition = new AchievementCondition("secondACondition", "a", 10);
-            AchievementCondition item = new AchievementCondition("sCondition", "s", 5);
-
-            Achievement a = new Achievement("a", "5xA", "You pressed a five times", achievementCondition);
-            //a.AchievementCompleted += AchievementCompleted;
-
-            Achievement b = new Achievement("b", "10xA", "You pressed a ten times", condition);
-            //b.AchievementCompleted += AchievementCompleted;
-
-            Achievement c = new Achievement("c", "5xS", "You pressed s five times", item);
-            //c.AchievementCompleted += AchievementCompleted;
-
-            Achievement d = new Achievement("d", "5xS+10xA", "You pressed s five times and a ten times",
-                new List<AchievementCondition> { condition, item });
-            //d.AchievementCompleted += AchievementCompleted;
-
-            Am.RegisterAchievement(a);
-            Am.RegisterAchievement(b);
-            Am.RegisterAchievement(c);
-            Am.RegisterAchievement(d);
-
-            Am.RegisterAchievementCondition(achievementCondition);
-            Am.RegisterAchievementCondition(condition);
-            Am.RegisterAchievementCondition(item);
-
-            Am.DeleteAchievementByUniqueId("d");
-            Am.DeleteAchievementByUniqueId("b");
-
-            Run();
-        }
-
-        static void AchievementCompleted(Achievement achievement)
-        {
-            Console.WriteLine();
-            Console.WriteLine("***** " + achievement.Titel + " *****");
-            Console.WriteLine("ACHIEVEMENT UNLOCKED: " + achievement.Description);
-        }
-
-        private static void Run()
-        {
-            ConsoleKeyInfo keyInfo;
-
-            do
-            {
-                keyInfo = Console.ReadKey();
-                Console.WriteLine();
-
-                if (keyInfo.Key == ConsoleKey.A)
-                {
-                    AchievementManager.GetInstance().ReportProgress("a");
-                }
-                else if (keyInfo.Key == ConsoleKey.S)
-                {
-                    AchievementManager.GetInstance().ReportProgress("s");
-                }
-            }
-            while (keyInfo.Key != ConsoleKey.Escape);
+            AchievementConditionTest.Execute();
         }
     }
 }
