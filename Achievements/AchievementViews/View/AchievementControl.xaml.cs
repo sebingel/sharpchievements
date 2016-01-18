6f66837b69766b8a99df82448bde567ee9e291ef
sebingel <sebingel+github@googlemail.com>
2016-01-18 16:15:14 +0100

    - AchievementControl now shows the progress in a nice ProgressBar with a string

--- Achievements/AchievementViews/View/AchievementControl.xaml.cs
+++ Achievements/AchievementViews/View/AchievementControl.xaml.cs
@@ -46,16 +46,20 @@ namespace sebingel.sharpchievements.AchievementViews.View
         /// </summary>
         public Visibility ImageVisibility { get; private set; }
 
+        /// <summary>
+        /// Progress in Percent
+        /// </summary>
         public int Progress { get; private set; }
 
+        /// <summary>
+        /// Show or hide the Progress
+        /// </summary>
         public Visibility ProgressVisibility { get; private set; }
 
         /// <summary>
-        /// A control to display an Achievement
+        /// Progress in x/y format
         /// </summary>
-        public AchievementControl()
-            : this(null)
-        { }
+        public string ProgressString { get; private set; }
 
         /// <summary>
         /// A control to display an Achievement
@@ -68,8 +72,10 @@ namespace sebingel.sharpchievements.AchievementViews.View
 
             InitializeComponent();
 
+            // Set the title
             Titel = achievement.Titel;
 
+            // set Description and Image according to the unlock status of the Achievement
             if (achievement.Unlocked)
             {
                 Description = achievement.Description;
@@ -81,15 +87,32 @@ namespace sebingel.sharpchievements.AchievementViews.View
                 ImagePath = "/sebingel.sharpchievements;component/Images/question30.png";
             }
 
+            // Show or hide the image
             if (String.IsNullOrEmpty(achievement.ImagePath))
                 ImageVisibility = Visibility.Collapsed;
             else
                 ImageVisibility = Visibility.Visible;
 
-            if (achievement.Conditions.Any() || achievement.Conditions.ToList()[0].CountToUnlock > 1)
+            // Display the progress
+            if (achievement.Conditions.Count() > 1 || achievement.Conditions.ToList()[0].CountToUnlock > 1)
             {
                 Progress = achievement.Progress;
                 ProgressVisibility = Visibility.Visible;
+
+                // calculate the overall Progress of the Achievement in a X/Y format
+                int overallNeeded = 0;
+                int overallProgress = 0;
+                foreach (AchievementCondition achievementCondition in achievement.Conditions)
+                {
+                    overallNeeded += achievementCondition.CountToUnlock;
+                    overallProgress += achievementCondition.ProgressCount;
+                }
+
+                // AchievementConditions never stop counting. But we don't want a progress of 20/2 displayed.
+                if (achievement.Unlocked)
+                    overallProgress = overallNeeded;
+
+                ProgressString = overallProgress + " / " + overallNeeded;
             }
             else
                 ProgressVisibility = Visibility.Collapsed;
@@ -100,16 +123,20 @@ namespace sebingel.sharpchievements.AchievementViews.View
             InvokePropertyChanged("ImageVisibility");
             InvokePropertyChanged("Progress");
             InvokePropertyChanged("ProgressVisibility");
+            InvokePropertyChanged("ProgressString");
         }
 
         #region INotifyPropertyChanged
+
         public event PropertyChangedEventHandler PropertyChanged;
+
         [NotifyPropertyChangedInvocator]
         private void InvokePropertyChanged([CallerMemberName] string propertyName = null)
         {
             if (PropertyChanged != null)
                 PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
         }
+
         #endregion
     }
 }
