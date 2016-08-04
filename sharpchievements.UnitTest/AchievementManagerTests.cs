2a22b0650291f703d4f237319393b330506043e6
sebingel <sebingel+github@googlemail.com>
2016-08-04 16:34:34 +0200

    Changed NUnit Version back to 2.6.4

---
+++ sharpchievements.UnitTest/AchievementManagerTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using sebingel.sharpchievements;
+
+namespace sharpchievements.UnitTest
+{
+    [TestFixture]
+    public class AchievementManagerTests
+    {
+        [Test]
+        public void Constructor_CreateInstance_InstanceCanBeCreated()
+        {
+            // Arrange
+            AchievementManager am;
+
+            // Act
+            am = new AchievementManager();
+
+            // Assert
+            Assert.IsNotNull(am);
+        }
+    }
+}
